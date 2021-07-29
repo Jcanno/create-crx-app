a447d7a61a6a0491f731e5c51d26d4428aee26d4
Jacano <641283231@qq.com>
2021-07-29 09:42:11 +0800

    feat: add dependencies

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 90,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+}
